daa4f0c2683bcc2a43b11fc8aec47eef6dbd3f57
Tyler Hartland <tyler.hartland@code42.com>
2014-09-05 14:33:36 -0500

    change wording of log message regarding view creation

--- lib/table_copy/copier.rb
+++ lib/table_copy/copier.rb
@@ -32,8 +32,8 @@ module TableCopy
         views = destination_table.query_views
         destination_table.drop(cascade: true)
         create_table
-        logger.info { "Creating #{views.count} views for #{destination_table.table_name}" }
         destination_table.create_views(views)
+        logger.info { "Created #{views.count} views for #{destination_table.table_name}" }
         moved_count = destination_table.copy_data_from(source_table)
         logger.info { "#{moved_count} rows moved to #{destination_table.table_name}" }
         destination_table.create_indexes
