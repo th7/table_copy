4512303f3bfd7bb924b7e651a90a2f366092fc52
Tyler Hartland <tyler.hartland@code42.com>
2014-09-22 11:55:20 -0500

    DDL module raises an error if source result has duplicate fields

--- lib/table_copy/pg/ddl.rb
+++ lib/table_copy/pg/ddl.rb
@@ -1,12 +1,22 @@
+require 'table_copy/error'
+
 module TableCopy
   module PG
     module DDL
+      class DuplicateField < TableCopy::Error; end
+
       class << self
         def for(pg_result, conn)
-          ddl_query = pg_result.fields.map do |field_name|
-            column_num = pg_result.fnumber(field_name)
+          used_fields = Set.new
+          ddl_query = pg_result.num_fields.times.map do |column_num|
             type_oid   = pg_result.ftype(column_num)
             type_mod   = pg_result.fmod(column_num)
+            field_name = pg_result.fname(column_num)
+
+            unless used_fields.add?(field_name)
+              raise DuplicateField, "\"#{field_name}\" used multiple times"
+            end
+
             "format_type(#{type_oid}, #{type_mod}) as #{field_name}"
           end.join(', ')
 
--- spec/lib/table_copy/pg/ddl_spec.rb
+++ spec/lib/table_copy/pg/ddl_spec.rb
@@ -2,7 +2,8 @@ require 'table_copy/pg/ddl'
 
 describe TableCopy::PG::DDL do
   let(:db) { DB.new }
-  let(:result) { db.with_conn { |c| c.exec("select * from #{db.table_name} limit 0") } }
+  let(:table1) { db.table_name }
+  let(:result) { db.with_conn { |c| c.exec("select * from #{table1} limit 0") } }
   let(:ddl) { TableCopy::PG::DDL }
   before { db.create_table }
   after  { db.drop_table }
@@ -11,6 +12,36 @@ describe TableCopy::PG::DDL do
     it 'returns the expected ddl' do
       db.with_conn do |conn|
         expect(ddl.for(result, conn)).to eq "column1 integer,\ncolumn2 character varying(123),\ncolumn3 character varying(256)[]"
+      end
+    end
+
+    context 'multiple tables' do
+      let(:table2) { 'table2' }
+      let(:result) { db.with_conn { |c| c.exec("#{select} from #{table1} inner join #{table2} on #{table1}.column1 = #{table2}.column1")} }
+
+      before { db.with_conn { |c| c.exec("create table #{table2} (column1 integer, column2 integer)") } }
+      after  { db.drop_table(table2) }
+
+      context 'with duplicate field' do
+        let(:select) { "select #{table1}.column2, #{table2}.column2" }
+
+        it 'raises a descriptive error' do
+          db.with_conn do |conn|
+            expect {
+              ddl.for(result, conn)
+            }.to raise_error TableCopy::PG::DDL::DuplicateField, /column2/
+          end
+        end
+      end
+
+      context 'without duplicate field' do
+        let(:select) { "select #{table1}.column2 t1_col2, #{table2}.column2 t2_col2" }
+
+        it 'returns the expected ddl' do
+          db.with_conn do |conn|
+            expect(ddl.for(result, conn)).to eq "t1_col2 character varying(123),\nt2_col2 integer"
+          end
+        end
       end
     end
   end
