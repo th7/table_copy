a7ced56b4a80c9d7e9b783d968c9ca9389e2e345
Tyler Hartland <tyler.hartland@code42.com>
2014-09-22 12:00:39 -0500

    Copier uses variable named "source" instead of "source_table"

--- spec/lib/table_copy_spec.rb
+++ spec/lib/table_copy_spec.rb
@@ -82,7 +82,7 @@ describe TableCopy do
 
       copier = tc.links[link_name]
       expect(copier).to be_kind_of TableCopy::Copier
-      expect(copier.source_table).to eq source
+      expect(copier.source).to eq source
       expect(copier.destination_table).to eq destination
     end
   end
